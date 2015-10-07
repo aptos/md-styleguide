371469285f837b7040a6935e33a47d625f0b0d41
Brian Wilkerson <bwilkerson@womply.com>
2015-10-07 16:38:49 -0700

    initial commit

---
+++ js/build-config.js
@@ -0,0 +1,7 @@
+DocsApp.constant('BUILDCONFIG', {
+  "ngVersion": "1.4.6",
+  "version": "0.11.2",
+  "repository": "https://github.com/angular/material",
+  "commit": "1d983a7672bfbcc5799e523890fbe707decae5b9",
+  "date": "2015-10-01 14:35:51 -0700"
+});
